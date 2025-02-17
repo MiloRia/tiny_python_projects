4b3afea04f01fb8ac23378a042f983bc8746252f
Maria Milovanova <MiloRia.lo@gmail.com>
2025-02-18 01:02:08 +0300

    add main files+folders+hello.py from 1st chapt

---
+++ 01_hello/hello.py
@@ -0,0 +1,6 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('-n','--name', metavar='name', default='World',help='Name to greet')
+args=parser.parse_args()
+print('Hello, '+ args.name +'!')
